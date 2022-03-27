dcc1e4e4496dc735f452d4e7ada6f5f89affedc4
ANAMIKA2107 <87655751+ANAMIKA2107@users.noreply.github.com>
2022-03-27 18:50:49 +0530

    Add files via upload

---
+++ 02-springboot-rest-jpa/src/main/java/com/bookapp/mod/exception/BookNotFoundException.java
@@ -0,0 +1,11 @@
+package com.bookapp.mod.exception;
+public class BookNotFoundException extends RuntimeException{
+
+	public BookNotFoundException(String message) {
+		super(message);
+		
+	}
+	
+}
+
+
